b8dd63e14b8b37ab6a312bbb2da7679c1798cbdd
Thomas Demoulin <thomasdem07@yahoo.be>
2022-01-17 20:57:45 +0100

    Form to share/sell

--- src/app/share-sell/share-sell.component.ts
+++ src/app/share-sell/share-sell.component.ts
@@ -1,5 +1,7 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
+import { StockObject } from '../table/table.component';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-share-sell',
@@ -9,17 +11,51 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ShareSellComponent implements OnInit {
 
   public itemForm: FormGroup;
-  @Input() data: any[];
+  @Input() data: StockObject[];
+
+  step: number = 0;
+  max: number = 0;
+
+  public statusList = [
+    "Sell", "Share",
+  ];
 
   constructor(private _formBuilder: FormBuilder) { }
-  
 
   ngOnInit(): void {
     this.itemForm = this._formBuilder.group({
       titleCtrl: ["", [Validators.required]],
-      amountCtrl: ["", [Validators.required, Validators.min(1)]],
+      amountCtrl: ["", [Validators.required]],
       statusCtrl: ["", [Validators.required]],
       dateCtrl: ["", [Validators.required]],
     });
+
+    this.itemForm.get('titleCtrl')?.valueChanges.subscribe((value: StockObject) => {
+      this.step = 1;
+      this.max = value.quantity;
+    });
+
+    this.itemForm.get('statusCtrl')?.valueChanges.subscribe(() => {
+      this.step = 2;
+    });
+
+    this.itemForm.get('amountCtrl')?.valueChanges.subscribe(() => {
+      this.step = 3;
+    });
+  }
+
+  onReset() {
+    this.itemForm.reset();
+    this.step = 0;
+  }
+
+  onSubmit() {
+    let title: StockObject = this.itemForm.get('titleCtrl')?.value;
+    let amount: number = this.itemForm.get('amountCtrl')?.value;
+    let status: string = this.itemForm.get('statusCtrl')?.value;
+    let date: Moment = this.itemForm.get('dateCtrl')?.value;
+    
+    this.data.push({name:title.name, quantity:amount, status:status, date:date.format("DD/MM/YYYY")});
+    this.onReset();
   }
 }
