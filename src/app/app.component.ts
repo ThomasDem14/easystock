74ff005b223c7cfb3e99469948ce1b76e4d7774c
Guillaume Lodrini <guillaume.lodrini@student.uliege.be>
2022-01-17 15:08:46 +0100

    menu color change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  homeColor: string = "#3d9f42";
+  historyColor: string = "#43557b";
+  profileColor: string = "#43557b";
+  shareColor: string = "#43557b";
+
   title = 'easystock';
   isShowing: boolean = true;
 
@@ -18,5 +23,31 @@ export class AppComponent {
 
   onMenuItemClick(item: string) {
     this.content = item;
+    switch (item) {
+      case 'home':
+        this.homeColor = "#3d9f42";
+        this.historyColor = "#43557b";
+        this.profileColor = "#43557b";
+        this.shareColor = "#43557b";
+        break;
+      case 'history':
+        this.historyColor = "#3d9f42";
+        this.homeColor = "#43557b";
+        this.profileColor = "#43557b";
+        this.shareColor = "#43557b";
+        break;
+      case 'share':
+        this.shareColor = "#3d9f42";
+        this.historyColor = "#43557b";
+        this.profileColor = "#43557b";
+        this.homeColor = "#43557b";
+        break;
+      case 'profile':
+        this.profileColor = "#3d9f42";
+        this.historyColor = "#43557b";
+        this.homeColor = "#43557b";
+        this.shareColor = "#43557b";
+        break;
+    }
   }
 }
